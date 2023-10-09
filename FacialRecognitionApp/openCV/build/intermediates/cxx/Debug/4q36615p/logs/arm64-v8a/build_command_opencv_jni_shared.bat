803fb45136df2d1d6a76e6b34b428b74c27d5eae
MarcosPaulo15 <marcos.machado20@outlook.com>
2023-10-08 22:00:11 -0300

    Commit inicial

---
+++ FacialRecognitionApp/openCV/build/intermediates/cxx/Debug/4q36615p/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Dell\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\Programas Java\\FacialRecognitionApp\\openCV\\.cxx\\Debug\\4q36615p\\arm64-v8a" ^
+  opencv_jni_shared
---
+++ FacialRecognitionApp/openCV/build/intermediates/cxx/Debug/4q36615p/logs/x86/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Dell\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\Programas Java\\FacialRecognitionApp\\openCV\\.cxx\\Debug\\4q36615p\\x86" ^
+  opencv_jni_shared
